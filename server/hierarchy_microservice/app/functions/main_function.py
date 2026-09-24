04a30f669c8d0e50b71e414c4de7d0e50b3b2706
theethenian <theethenian@gmail.com>
2026-09-24 16:32:21 +0300

    remodel

---
+++ server/hierarchy_microservice/app/functions/main_function.py
@@ -0,0 +1,80 @@
+######################################################
+
+def group_read(user_id, data):
+        #check if group is pubic or if user is member 
+        pass
+
+def group_create(user_id, data):
+        #check if group name is taken 
+        pass
+
+def group_update(user_id, data):
+        #check if user is group creator 
+        pass
+
+def group_delete(user_id, data):
+        #check if user is creator
+        pass
+
+######################################################
+
+def folder_read(user_id, data):
+        #check if group/folder is pubic or if user is allowed member 
+        pass
+
+def folder_create(user_id, data):
+        #check if user is group creator 
+        #check if folder name is taken  withing group
+        pass
+
+def folder_update(user_id, data):
+        #check if user is folder admin 
+        pass
+
+def folder_delete(user_id, data):
+        #check if user is creator 
+        pass
+
+
+######################################################
+
+def note_read(user_id, data):
+        #check if group/folder/note is pubic or if user is member 
+        #check if user is note member with privillages 
+        pass
+
+def note_create(user_id, data):
+        #check if note name is taken within the folder 
+        pass
+
+def note_update(user_id, data):
+        #check if user is note member with privillages 
+        pass
+
+def note_delete(user_id, data):
+        #check if user is creator/admin 
+        pass
+
+######################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
